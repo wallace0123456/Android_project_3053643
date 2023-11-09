ed30835e6ec683b6cb53ff48eda9b4d4f8c71bc3
Wally <chihin.chan@student.griffith.ie>
2023-11-09 23:44:37 +0000

    Change the background for login page

--- app/src/main/java/com/griffith/md_project_3053643/Login.kt
+++ app/src/main/java/com/griffith/md_project_3053643/Login.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +20,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat.startActivity
@@ -49,26 +52,30 @@ class Login : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginPage(navController: NavController) {
+    Image(painter = painterResource(id = R.drawable.bg),
+        contentDescription = "background",
+        contentScale = ContentScale.FillBounds,
+    )
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
       Text(text = "Login CADA",
            style = MaterialTheme.typography.bodyLarge,
-           modifier = Modifier.padding(bottom = 16.dp, top = 16.dp)
+           modifier = Modifier.padding(bottom = 26.dp, top = 26.dp)
       )
       TextField(value ="" ,
           onValueChange ={ } ,
           label = { Text("Username")},
-          modifier = Modifier.padding(bottom = 16.dp)
+          modifier = Modifier.padding(bottom = 20.dp)
       )
       TextField(value = "",
           onValueChange ={},
           label={ Text("Password")},
-          modifier = Modifier.padding(bottom = 16.dp)
+          modifier = Modifier.padding(bottom = 20.dp)
       )
       Button(
-          onClick = {navController.navigate("Main")},
+          onClick = {},
           modifier = Modifier
               .fillMaxWidth()
               .padding(bottom = 16.dp)
